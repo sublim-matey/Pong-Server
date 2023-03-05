e0a8431c9d8ce0a374caa3b404671b9111fa08f5
Max H <maxfieldbonafacio@gmail.com>
2023-03-04 19:03:19 -0800

    Added Server module

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,4 +27,5 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation("io.ktor:ktor-network:$ktor_version")
 }
--- src/main/kotlin/com/nonnewtonian/Application.kt
+++ src/main/kotlin/com/nonnewtonian/Application.kt
@@ -4,13 +4,57 @@ import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.nonnewtonian.plugins.*
+import io.ktor.network.selector.*
+import io.ktor.network.sockets.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import io.ktor.utils.io.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
+
+// Create Pong executable
+// Create Pong output - send to client
+// Create async listener for input
+// Input needs to have a buffer, we'll use a queue of Ints
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
-        .start(wait = true)
+//    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+//        .start(wait = true)
+
+    runBlocking {
+        val selectorManager = SelectorManager(Dispatchers.IO)
+        val serverSocket = aSocket(selectorManager).tcp().bind("127.0.0.1", 9002)
+        println("Server is listening at ${serverSocket.localAddress}")
+        while (true) {
+            val socket = serverSocket.accept()
+            println("Accepted $socket")
+            launch {
+                val receiveChannel = socket.openReadChannel()
+                val sendChannel = socket.openWriteChannel(autoFlush = true)
+                sendChannel.writeStringUtf8("Please enter your name\n")
+                try {
+                    while (true) {
+                        val name = receiveChannel.readUTF8Line()
+                        sendChannel.writeStringUtf8("Hello, $name!\n")
+                    }
+                } catch (e: Throwable) {
+                    socket.close()
+                }
+            }
+        }
+    }
 }
 
 fun Application.module() {
     configureSockets()
     configureRouting()
+}
+
+fun Application.configureRouting() {
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
 }
